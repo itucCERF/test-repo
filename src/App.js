ce4bd4992091a037fb2d3fd9d275a704504a15fe
Duong Dinh Tuan <tuandd99.nta@gmail.com>
2024-08-26 11:18:21 +0700

    [Add]: test commit cherry

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>Edit <code>src/App.js</code> and save to reload.123</p>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
+                    Learn React123123123
                 </a>
             </header>
         </div>
